90389bbd2f559a25ba0a1900cf1b65597f8d5ae4
Yoshifumi Kanno <yk2100867@gmail.com>
2017-12-16 12:20:54 +0900

    add pug

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'pug');
+
 app.listen(process.env.PORT || 3000, function(){
   console.log("server listen");
 });
 
 app.get('/', function(req, res){
-  res.send("hello world123");
+  res.render('index', {title: 'タイトル', message: 'メッセージ'});
 })

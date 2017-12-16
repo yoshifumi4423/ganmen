75620ecb3e1058b1e78ccd6f07cae7216778baa0
Yoshifumi Kanno <yk2100867@gmail.com>
2017-12-16 11:21:14 +0900

    add hello world

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.listen(3000, function(){
+  console.log("server listen");
+});
+
+app.get('/', function(req, res){
+  res.send("hello world");
+})

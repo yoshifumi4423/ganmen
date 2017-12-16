828d6fd907224b6e4a710906f9b2f2bb72fde0b2
Yoshifumi Kanno <yk2100867@gmail.com>
2017-12-16 11:41:51 +0900

    set PORT

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("server listen");
 });
 

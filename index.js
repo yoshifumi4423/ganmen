aa7cc46ec17850107c643a70bf86d14eefb71b53
Yoshifumi Kanno <yk2100867@gmail.com>
2017-12-16 14:46:53 +0900

    post images

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
-
 const app = express();
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'});
 
 app.set('view engine', 'pug');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('uploads'));
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("server listen");
@@ -10,4 +15,9 @@ app.listen(process.env.PORT || 3000, function(){
 
 app.get('/', function(req, res){
   res.render('index', {title: 'タイトル', message: 'メッセージ'});
+})
+
+app.post('/images', upload.single('faceImage'), function(req, res){
+  console.log(req.file);
+  res.send('res_send');
 })

41a18efd3a4341681ef92e630dd45fa02107db2c
Yoshifumi Kanno <yk2100867@gmail.com>
2018-05-27 17:16:26 +0900

    Add Styled-Components.

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,4 +1,13 @@
 import React from "react"
+import styled from "styled-components"
+
+const Image = styled.div`
+  position: absolute;
+  max-width: 500px;
+  img {
+    width: 100%;
+  }
+ `
 
 // reactのcomponent
 class Images extends React.Component {
@@ -24,7 +33,7 @@ class Images extends React.Component {
       <div>
         {
           this.state.images.map((image) => {
-            return <img src={image.filename} key={image.id}/>
+            return <Image key={image.id}><img src={image.filename}/></Image>
           })
         }
       </div>

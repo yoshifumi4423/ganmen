8dbb707e27a8eb65b598c7984ab4d9dd8f5df4c0
u9r52sld <u9r52sld@gmail.com>
2018-06-03 18:36:13 +0900

    fix images query

--- routes/api/images.js
+++ routes/api/images.js
@@ -5,25 +5,24 @@ const auth = require('../../middlewares/auth')
 
 router.get('/images', auth, (req, res) => {
   models.Image.findAll({
-    // 条件1：自分の画像でない
     where: {
+      // 条件1：自分の画像でない
       userId: {
         $not: req.user.id
+      },
+      // 条件2：未評価の画像
+      '$"Ratings"."userId"$': {
+        $or: {
+          $not: req.user.id,
+          $eq: null,
+        }
       }
     },
-    // 条件2：未評価の画像
     include: [{
       model: models.Rating,
       required: false,
-      where: {
-        userId: {
-          $or: {
-            $not: req.user.id,
-            $eq: null
-          }
-        }
-      }
     }],
+    subQuery: false,
     // 条件3：10個
     limit: 10,
   })

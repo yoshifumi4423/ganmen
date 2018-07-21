fd656ef5b5c0e8c34eab83ac2b56bb87b0efe8ee
Yoshifumi Kanno <yk2100867@gmail.com>
2018-07-21 19:02:04 +0900

    Change image-upload

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,5 +1,30 @@
 'use strict'
+const fs = require('fs-extra')
 const multer = require('multer')
+const generateToken = require('../utils/generateToken')
+const path = require('path')
+const sharp = require('sharp')
+
+// 画像の保存先とファイル名を指定
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const date = new Date()
+    const year = date.getFullYear()
+    const month = ("0" + (date.getMonth() + 1)).slice(-2)
+    const day = ("0" + date.getDate()).slice(-2)
+
+    const uploadPath = `uploads/${year}/${month}/${day}`
+
+    fs.mkdirs(uploadPath).then(() => {
+      cb(null, uploadPath)
+    }).catch(cb)
+  },
+  filename: (req, file, cb) => {
+    generateToken(16).then((token) => {
+      cb(null, token + path.extname(file.originalname))
+    }).catch(cb)
+  }
+})
 
 // 指定の画像ファイルのみを受け付ける
 const fileFilter = (req, file, cb) => {
@@ -10,9 +35,35 @@ const fileFilter = (req, file, cb) => {
 }
 
 // ミドルウェア
-const upload = multer({
-  dest: 'uploads/',
+const multerMiddleware = multer({
+  storage: storage,
   fileFilter: fileFilter,
 }).single('faceImage')
+
+// 画像のリサイズとfileオブジェクトの整形
+const upload = (req, res, next) => {
+  multerMiddleware(req, res, () => {
+    if (!req.file) return next()
+
+    const originalPath = req.file.path
+    const thumbnailPath = originalPath.replace(/(.*)(\..*)$/, '$1-640x640$2')
+    sharp(originalPath).resize(640, 640).toFile(thumbnailPath, (err, info) => {
+      if (err) return next(err)
+
+      const pathToUrl = (path) => {
+        return path.replace(/^uploads/, `${req.protocol}://${req.get('host')}`)
+      }
+
+      req.file = {
+        mimetype: req.file.mimetype,
+        originalUrl: pathToUrl(originalPath),
+        thumbnailUrl: pathToUrl(thumbnailPath)
+      }
+
+      next()
+    })
+  })
+}
+
 
 module.exports = upload
---
+++ utils/generateToken.js
@@ -0,0 +1,12 @@
+const crypto = require("crypto")
+
+const generateToken = (size) => {
+  return new Promise((resolve, reject) => {
+    crypto.randomBytes(size, (err, buf) => {
+      if (err) reject(err)
+      resolve(buf.toString('hex'))
+    })
+  })
+}
+
+module.exports = generateToken

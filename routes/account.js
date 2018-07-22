8cc8c73234964313f46a22196250b14d4253aae0
Yoshifumi Kanno <yk2100867@gmail.com>
2018-07-22 20:27:47 +0900

    Make new routes for 'account/email' and 'account/password'.

--- routes/account.js
+++ routes/account.js
@@ -17,7 +17,25 @@ router.get('/', auth, (req, res) => {
   })
 })
 
-router.post('/', auth, (req, res) => {
+router.post('/email', auth, (req, res) => {
+  if (!req.user) {
+    return res.send('error: please login.')
+  }
+
+  req.user.update(
+    { email: req.body.email },
+    { returning: true } // 変更後のデータを返すかどうか
+  ).then((userObj) => {
+    res.render('account', {
+      form: {
+        email: userObj.dataValues.email,
+      },
+      errors: []
+    })
+  })
+})
+
+router.post('/password', auth, (req, res, next) => {
   if (!req.user) {
     return res.send('error: please login.')
   }
@@ -26,51 +44,40 @@ router.post('/', auth, (req, res) => {
   const newPw = req.body.newPassword
   const newPwConfirmation = req.body.newPasswordConfirmation
 
-  if (currentPw || newPw || newPwConfirmation) {
-    bcrypt.compare(currentPw, req.user.password).then((same) => {
-      if (!same) {
-        return res.render('account', {
-          form: {
-            email: req.body.email,
-          },
-          errors: ["正しい現在のパスワードを入力してください。"]
-        })
-      }
-      if (newPw.length < 8 || newPwConfirmation < 8) {
-        return res.render('account', {
-          form: {
-            email: req.body.email,
-          },
-          errors: ["新しいパスワードを8文字以上で入力してください。"]
-        })
-      }
-      if (newPw !== newPwConfirmation) {
-        return res.render('account', {
-          form: {
-            email: req.body.email
-          },
-          errors: ["新しいパスワードと新しいパスワードの確認を等しくしてください。"]
-        })
-      }
-
-      bcrypt.hash(newPw, 10).then((hash) => {
-        req.user.update({
-          email: req.body.email,
-          password: hash
-        })
-      })
-    })
-  } else {
-    req.user.update({
-      email: req.body.email
-    })
+  if (!currentPw && !newPw && !newPwConfirmation) {
+    req.errors = ["正しい現在のパスワードを入力してください。"]
+    return next()
   }
 
+  bcrypt.compare(currentPw, req.user.password).then((same) => {
+    if (!same) {
+      req.errors = ["正しい現在のパスワードを入力してください。"]
+      return next()
+      }
+    else if (newPw.length < 8 || newPwConfirmation < 8) {
+      req.errors = ["新しいパスワードを8文字以上で入力してください。"]
+      return next()
+    }
+    else if (newPw !== newPwConfirmation) {
+      req.errors = ["新しいパスワードと新しいパスワードの確認を等しくしてください。"]
+      return next()
+    }
+
+    bcrypt.hash(newPw, 10).then((hash) => {
+      req.user.update({
+        password: hash
+      }).then((userObj) => {
+        req.errors = []
+        return next()
+      })
+    })
+  })
+}, (req, res) => {
   res.render('account', {
     form: {
-      email: req.body.email,
+      email: req.user.email
     },
-    errors: []
+    errors: req.errors
   })
 })
 

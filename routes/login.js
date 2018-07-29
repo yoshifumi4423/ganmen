2154a4b581b3546550bf699512aa86da0580e113
Yoshifumi Kanno <yk2100867@gmail.com>
2018-07-29 17:04:50 +0900

    Restore AppError.

--- routes/login.js
+++ routes/login.js
@@ -1,33 +1,51 @@
 'use strict'
 const express = require('express')
+const router = express.Router()
 const models = require('../models')
 const bcrypt = require("bcrypt")
 const auth = require('../middlewares/auth')
 const logoutChecker = require('../middlewares/logoutChecker')
-const router = express.Router()
+const AppError = require('../errors/AppError')
 
 router.use(auth)
 router.use(logoutChecker)
 
-router.get('/', function(req, res){
-  console.log("session : ", req.session.user)
-  res.render('login')
+router.get('/', function(req, res){	
+  console.log("session : ", req.session.user)	
+  res.render('login', {	
+    form: {},	
+    errors: []	
+  })	
 })
 
-router.post('/', function(req, res){
-  console.log("session : ", req.session.user)
-  models.User.findOne({
-    where:{
-      email:req.body.email
+router.post('/', function(req, res, next){	
+  console.log("session : ", req.session.user)	
+  models.User.findOne({	
+    where:{	
+      email: req.body.email	
     }
   }).then((user) => {
-    bcrypt.compare(req.body.password, user.password).then((same) => {
-      req.session.user_id = user.id
-      res.send('res_send')
+    if (!user) {	
+      throw new AppError("メールアドレスまたはパスワードが間違っています。")	
+    }	
+     return bcrypt.compare(req.body.password, user.password).then((result) => {	
+      if (!result) {	
+        throw new AppError("メールアドレスまたはパスワードが間違っています。")	
+      }	
+       req.session.user_id = user.id	
+      res.send('res_send')	
     })
   }).catch((errorObj) => {
-    
-  })
+    if (errorObj.name === 'AppError') {	
+      return res.render('login', {	
+        form: {	
+          email: req.body.email	
+        },	
+        errors: [errorObj.message]	
+      })	
+    }	
+    return next(errorObj)	
+  })	
 })
 
 module.exports = router

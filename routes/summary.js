f8b1c2a5d96d74da4f5220efe56615a5cbae893a
Yoshifumi Kanno <yk2100867@gmail.com>
2018-07-31 00:08:33 +0900

    Add a new route 'summary' to show rating summary of user's images.

---
+++ routes/summary.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+const auth = require('../middlewares/auth')
+const loginChecker = require('../middlewares/loginChecker')
+
+router.use(auth)
+router.use(loginChecker)
+
+router.get('/', (req, res) => {
+  res.render('summary', {
+
+  })
+})
+
+module.exports = router

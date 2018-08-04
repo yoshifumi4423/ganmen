120fc89c682aa801e274dced6b455aedf9007c15
Yoshifumi Kanno <yk2100867@gmail.com>
2018-08-04 22:39:44 +0900

    Change sequelize query to exclude tables which user gave rating.

--- routes/api/images.js
+++ routes/api/images.js
@@ -2,33 +2,41 @@ const express = require('express')
 const router = express.Router()
 const models = require('../../models')
 const auth = require('../../middlewares/auth')
+const sequelize = require('sequelize')
 
-router.get('/images', auth, (req, res) => {
+router.get('/images', auth, (req, res, next) => {
   models.Image.findAll({
     where: {
       // 条件1：自分の画像でない
       userId: {
         $not: req.user.id
       },
-      // 条件2：未評価の画像
-      '$"Ratings"."userId"$': {
-        $or: {
-          $not: req.user.id,
-          $eq: null,
-        }
+      // 条件2：自分が評価した画像は対象外
+      id: {
+        $notIn: sequelize.literal(
+          '(select "imageId" from "Ratings" where "userId" = ' + req.user.id + ')'
+        )
       }
     },
     include: [{
       model: models.Rating,
-      required: false,
+      required: false,  // left outer join
+      where: {
+        // 条件3：未評価の画像
+        userId: {
+          $or: {
+            $not: req.user.id,
+            $eq: null,
+          }
+        }, 
+      }
     }],
     subQuery: false,
-    // 条件3：10個
+    // 条件4：10個
     limit: 10,
-  })
-  .then((images) => {
+  }).then((images) => {
     res.json({images: images})
-  })
+  }).catch(errorObj => next(errorObj))
 })
 
 module.exports = router

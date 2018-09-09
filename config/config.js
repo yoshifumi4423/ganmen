eaab7cb77ec14cad1c437797cec39382896d6fe6
Yoshifumi Kanno <yk2100867@gmail.com>
2018-09-09 17:14:51 +0900

    Add port for development-environment.

--- config/config.js
+++ config/config.js
@@ -3,12 +3,15 @@ require('dotenv').config()
 
 module.exports = {
   development: {
-    use_env_variable: "DEV_DB_URL"
+    use_env_variable: "DEV_DB_URL",
+    use_env_variable_port: "DEV_PORT"
   },
   test: {
-    use_env_variable: "DEV_DB_URL"
+    use_env_variable: "DEV_DB_URL",
+    use_env_variable_port: "DEV_PORT"
   },
   production: {
-    use_env_variable: "PROD_DB_URL"
+    use_env_variable: "PROD_DB_URL",
+    use_env_variable_port: "PROD_PORT"
   }
 }
